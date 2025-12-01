351f2909e69f2df9b09c32e1ce859e9a560c908d
tDog - Tini <martynas.raiselis@gmail.com>
2025-12-01 16:46:31 +0100

    fix suspense, for testing in prod

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,37 +1,46 @@
-import { Await, createFileRoute } from '@tanstack/react-router'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { createFileRoute } from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/react-start'
 import { Suspense } from 'react'
 import { Skeleton } from '@/components/ui/skeleton'
 
 export const Route = createFileRoute('/')({
-    loader: async () => {
-        return {
-            slow: new Promise<string>((r) => setTimeout(() => r('later!'), 3000)),
-        }
-    },
     component: Page,
 })
 
-function Page() {
-    const data = Route.useLoaderData()
+const loaderFn = createServerFn().handler(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 3000))
+    return { msg: 'This is loaded data!' }
+})
 
+export function Page() {
     return (
         <div>
             <Suspense fallback={<SkeletonDemo />}>
-                <div className="flex items-center space-x-4">
-                    <div className="border h-12 w-12 rounded-full"></div>
-                    <div className="space-y-2">
-                        <div className="border w-[250px]">
-                            <Await promise={data.slow}>{(value) => <p>{value}</p>}</Await>
-                        </div>
-                        <div className="border w-[200px]"></div>
-                    </div>
-                </div>
+                <Data />
             </Suspense>
         </div>
     )
 }
 
-export function SkeletonDemo() {
+function Data() {
+    const { data } = useSuspenseQuery({
+        queryKey: ['data'],
+        queryFn: () => loaderFn(),
+    })
+
+    return (
+        <div className="flex items-center space-x-4">
+            <div className="border h-12 w-12 rounded-full"></div>
+            <div className="space-y-2">
+                <div className="border w-[250px]">{data.msg}</div>
+                <div className="border w-[200px]"></div>
+            </div>
+        </div>
+    )
+}
+
+function SkeletonDemo() {
     return (
         <div className="flex items-center space-x-4">
             <Skeleton className="h-12 w-12 rounded-full" />
